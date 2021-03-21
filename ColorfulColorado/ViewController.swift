bc4376275adc59caabd4872054733ca0a5a9bdb4
Gabo Cuadros <gabo-jp@outlook.com>
2021-03-21 03:03:38 -0500

    Initial Commit

---
+++ ColorfulColorado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorfulColorado
+//
+//  Created by Gabo Cuadros Cardenas on 3/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ColorfulColoradoTests/ColorfulColoradoTests.swift
@@ -0,0 +1,33 @@
+//
+//  ColorfulColoradoTests.swift
+//  ColorfulColoradoTests
+//
+//  Created by Gabo Cuadros Cardenas on 3/21/21.
+//
+
+import XCTest
+@testable import ColorfulColorado
+
+class ColorfulColoradoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
